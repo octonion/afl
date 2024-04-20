841f0d1ca5392b5f07644408f49ee19dca4778b9
Christopher D. Long <galizur@gmail.com>
2024-04-19 20:38:32 -0400

    AFL update.

--- sos/lmer.R
+++ sos/lmer.R
@@ -19,11 +19,11 @@ r.team_score::float as gs,
 --(case when r.year<2021 then 0.80*r.team_score::float
 --      else r.team_score::float
 --end) as gs,
-(year-2022) as w
+(year-2023) as w
 from afl.results r
 
 where
-    r.year between 2023 and 2024
+    r.year between 2024 and 2024
 
 ;")
 
